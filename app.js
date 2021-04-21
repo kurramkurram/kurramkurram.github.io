9df3d88cc0e84ed07d64e2580bb482e0164aa29c
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2021-04-21 23:59:47 +0900

    set top app bar

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
-import {MDCRipple} from '@material/ripple';
-
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
-
-const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
-iconButtonRipple.unbounded = true;
-
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
-});
+console.log('hello world');
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,6 @@
 const autoprefixer = require('autoprefixer');
 
-const path = require('path');
-
-function tryResolve_(url, sourceFilename) {
-  // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
-  // when the importer throws
-  try {
-    return require.resolve(url, {paths: [path.dirname(sourceFilename)]});
-  } catch (e) {
-    return '';
-  }
-}
-
-function tryResolveScss(url, sourceFilename) {
-  // Support omission of .scss and leading _
-  const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
-  return tryResolve_(normalizedUrl, sourceFilename) ||
-    tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
-      sourceFilename);
-}
-
-function materialImporter(url, prev) {
-  if (url.startsWith('@material')) {
-    const resolved = tryResolveScss(url, prev);
-    return {file: resolved || url};
-  }
-  return {file: url};
-}
-
 module.exports = {
-  mode: 'production',
   entry: ['./app.scss', './app.js'],
   output: {
     filename: 'bundle.js',
@@ -59,14 +30,13 @@ module.exports = {
           },
           {
             loader: 'sass-loader',
-            options: {   
+            options: {
               // Prefer Dart Sass
               implementation: require('sass'),
-         
+
               // See https://github.com/webpack-contrib/sass-loader/issues/804
               webpackImporter: false,
               sassOptions: {
-                importer: materialImporter,
                 includePaths: ['./node_modules'],
               },
             },
