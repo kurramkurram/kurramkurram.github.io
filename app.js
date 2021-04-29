a8855a2301cf6ed842537c9aa02f8671e38a11aa
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2021-04-29 18:04:37 +0900

    remove unnecessary comments

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ jQuery(function() {
     var appear = false;
     var pagetop = $('#page_top');
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if ($(this).scrollTop() > 100) {
         if (appear == false) {
           appear = true;
           pagetop.stop().animate({
-            'bottom': '50px' //下から50pxの位置に
-          }, 300); //0.3秒かけて現れる
+            'bottom': '50px'
+          }, 300);
         }
       } else {
         if (appear) {
           appear = false;
           pagetop.stop().animate({
-            'bottom': '-50px' //下から-50pxの位置に
-          }, 300); //0.3秒かけて隠れる
+            'bottom': '-50px'
+          }, 300);
         }
       }
     });
     pagetop.click(function () {
-      $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+      $('body, html').animate({ scrollTop: 0 }, 500);
       return false;
     });
   }
