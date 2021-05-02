b914a5f55911c369c45007124ad09d96b29f3c2c
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2021-05-02 17:45:47 +0900

    refactor go to top js

--- app.js
+++ app.js
@@ -4,32 +4,7 @@ import {MDCTopAppBar} from '@material/top-app-bar';
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
-jQuery(function() {
-    var appear = false;
-    var pagetop = $('#page_top');
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        if (appear == false) {
-          appear = true;
-          pagetop.stop().animate({
-            'bottom': '50px'
-          }, 300);
-        }
-      } else {
-        if (appear) {
-          appear = false;
-          pagetop.stop().animate({
-            'bottom': '-50px'
-          }, 300);
-        }
-      }
-    });
-    pagetop.click(function () {
-      $('body, html').animate({ scrollTop: 0 }, 500);
-      return false;
-    });
-  }
-);
+
 
 new ScrollObserver('.carrier__wrapper');
 new ScrollObserver('.skill__wrapper');
